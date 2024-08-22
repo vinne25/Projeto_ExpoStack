593115d9f49722b5b386aaed1ad8eda322b4bb6d
vinne25 <vinicius.silva.leite@hotmail.com>
2024-08-21 21:50:31 -0300

    first comit

--- app/(drawer)/(tabs)/_layout.tsx
+++ app/(drawer)/(tabs)/_layout.tsx
@@ -1,7 +1,9 @@
 import { Tabs } from 'expo-router';
+//importa o componente tabs do modulo 'expo-router', que é usado para criar uma navegação de abas na aplicação
 
 import { TabBarIcon } from '~/components/TabBarIcon';
-
+//importa o componente 'tabbaricon' de um caminho relativo, este componente será usado para rederizar icones personalizados na barra de aba
+  
 export default function TabLayout() {
   return (
     <Tabs
@@ -9,18 +11,29 @@ export default function TabLayout() {
         headerShown: false,
         tabBarActiveTintColor: 'black',
       }}>
+        {/*
+          o componente tabs organiza a navegação de app em layout de abas
+          as opções screenoptions configuram o comportamento e a aparencia das abas
+          headershown: falsa esconde o cabeçalho padrão de navegação
+          tabbaractiveintcolor: black define a cor do texto e dos icones das abas ativas como preto
+       */}
       <Tabs.Screen
         name="index"
         options={{
           title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => <TabBarIcon name="apple" color={color} />,
         }}
       />
+      {/**
+        define a primeria aba com o nome da rota index
+        title: tab one define o titulo da aba
+        tabbaricon: utiliza o componente tabbaricon ára rederizar um icone 
+    */}
       <Tabs.Screen
         name="two"
         options={{
           title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => <TabBarIcon name="android" color={color} />,
         }}
       />
     </Tabs>
--- app/(drawer)/(tabs)/index.tsx
+++ app/(drawer)/(tabs)/index.tsx
@@ -1,7 +1,11 @@
 import { Stack } from 'expo-router';
+//importa o componente stack do modulo expo-router, usado para configurar a navegação em pilha (stack navigation)
 
 import { Container } from '~/components/Container';
+//importa o componente container de um caminho relativo, geralmente usado para organizar o centralizar o conteudo
+
 import { ScreenContent } from '~/components/ScreenContent';
+//importa o componente que provavelmente e responsavel por exibir o conteudo esperado, como textos e elementeos visuais 
 
 export default function Home() {
   return (
